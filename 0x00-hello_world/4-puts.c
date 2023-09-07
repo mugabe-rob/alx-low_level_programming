af4f03859ce5f57e88cb9ff4c2be6efbf6043cfa
mugabe-rob <mugaberobert222004644@gmail.com>
2023-09-07 02:39:49 -0700

    Printing a first c programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that prints aline with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
