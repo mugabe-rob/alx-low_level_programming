0a38018910b5c8155ad6089f54535cba55c856bd
mugabe-rob <mugaberobert222004644@gmail.com>
2023-09-12 06:00:05 -0700

    A program to print alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
+
+
