97e18c99928a753eaacba703363b2db86f4e997d
mugabe-rob <mugaberobert222004644@gmail.com>
2023-09-13 09:05:40 -0700

    Mujltiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - A function to see whether positive or negative
+* main : A function to see whether positive or negative
 * Return: 0
 */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	positive_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
