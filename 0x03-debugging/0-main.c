20f7e3ace78f010a4ed05f2d95a22751f81d6d4e
mugabe-rob <mugaberobert222004644@gmail.com>
2023-09-13 08:00:42 -0700

    Aprogram to display multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
 * main - A function to see whether positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-int n;
-n = 0;
-positive_or_negative(n);
-return (0);
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
 }
