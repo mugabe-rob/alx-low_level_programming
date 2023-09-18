6e38b445a595cb9e464fadf7f2da17fac0164b49
mugabe-rob <mugaberobert222004644@gmail.com>
2023-09-18 05:37:13 -0700

    A function to printin reverse form

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  * print_rev - print it revesely
+ *   * @a: string
+ *    * return: 0
+ *     */
+void print_rev(char *a)
+{
+		int longi = 0;
+			int o;
+
+				while (*a != '\0')
+						{
+									longi++;
+											a++;
+												}
+					a--;
+						for (o = longi; o > 0; o--)
+								{
+											_putchar(*a);
+													a--;
+														}
+
+							_putchar('\n');
+}
+
